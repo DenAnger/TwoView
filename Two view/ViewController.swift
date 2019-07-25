c922eaf0804515e378b4896fac77e0c14f89d7af
Abramov Denis <denmiracle@mail.ru>
2019-07-25 13:22:30 +0700

    Sum
    1. Add two view
    2. Added constraints that views were located when changing orientation of the screen.

---
+++ Two view/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Two view
+//
+//  Created by Denis Abramov on 24.07.2019.
+//  Copyright © 2019 Denis Abramov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
