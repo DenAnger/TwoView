95b1b99bf36abf624ee2e6fdf5361a6161dc8ae1
Denis Abramov <DenAnger@outlook.com>
2021-03-18 22:43:04 +0700

    update to recommended settings

---
+++ Two view/Supports/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Two view
+//
+//  Created by Denis Abramov on 24.07.2019.
+//  Copyright © 2019 Denis Abramov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
